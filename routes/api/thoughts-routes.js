45ebcef92db2640de48a486abf55adb9541c81ae
Bayleigh Walker <79384523+bayleighwalker@users.noreply.github.com>
2021-08-02 16:21:17 -0500

    Update thoughts-routes.js

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -12,17 +12,15 @@ const {
 // /api/thoughts
 router.route("/").get(getAllThoughts).post(createThought);
 
-// /api/thoughts/:id
 router
 	.route("/:id")
 	.get(getthoughtById)
 	.put(updateThought)
 	.delete(deleteThought);
 
-//	/api/thoughts/:id/reactions
 router.route("/:id/reactions").post(createReaction);
 
-// /api/thoughts/:id/reactions/:reactionId
+
 router.route("/:id/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
