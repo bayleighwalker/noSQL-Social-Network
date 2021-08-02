0e188ee5fad7b2131bb1d8fd3cbf350082d10da1
Bayleigh Walker <79384523+bayleighwalker@users.noreply.github.com>
2021-08-02 16:21:58 -0500

    Update user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,13 +10,10 @@ const {
 	deleteFriend,
 } = require("../../controllers/user-controller");
 
-// /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
-// /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
-// /api/users/:id/friends/:friendsId
 router.route("/:id/friends/:friendsId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
